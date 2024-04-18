b11a7f3005e8c575100958dbc482935d6834c2a3
Yabiti <yabetseyohannes@gmail.com>
2024-04-18 15:04:50 +0300

    .

--- 0x06-starwars_api/yab.py
+++ 0x06-starwars_api/yab.py
@@ -8,3 +8,8 @@ na = figlet.getFonts()
 
 if len(sys.argv) == 0:
     random.shuffle(na)
+elif len(sys.argv) == 2:
+    for s in na:
+        print(figlet.renderText(s))
+else:
+    sys.exit("Error")
