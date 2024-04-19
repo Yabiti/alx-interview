9bb7bf2a7c96a184d59c666e6059de1576d87dce
Yabiti <yabetseyohannes@gmail.com>
2024-04-19 12:35:45 +0300

    .

--- 0x06-starwars_api/yab2.py
+++ 0x06-starwars_api/yab2.py
@@ -1,16 +1,18 @@
 import random
 
 i = 100
-n = int(input("Guess: "))
-while i < 1:
+n = input("Guess: ")
+while True:
     if i < 1:
-        n = int(input("Guess: "))
+        n = input("Guess: ")
     elif i == n:
         print("just right!")
+        break
     elif i > n:
         print("Too large")
+        n = input("Guess: ")
     else:
         print("Too small")
+        n = input("Guess: ")
 
 i = random.randint(1,100)
-print(i)
