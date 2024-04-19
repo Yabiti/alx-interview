085af93cf9d57933a0a663d1b508786bac8b0839
Yabiti <yabetseyohannes@gmail.com>
2024-04-19 09:52:33 +0300

    .

--- 0x06-starwars_api/yab1.py
+++ 0x06-starwars_api/yab1.py
@@ -1,13 +1,7 @@
-import sys
-songs  = [
-    Adieu, adieu, to Liesl
-Adieu, adieu, to Liesl and Friedrich
-Adieu, adieu, to Liesl, Friedrich, and Louisa
-Adieu, adieu, to Liesl, Friedrich, Louisa, and Kurt
-Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, and Brigitta
-Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, and Marta
-Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, Marta, and Gretl
-]
-
-for songs in sys.argv[1:]:
-    print()
+#Adieu, adieu, to Liesl
+#Adieu, adieu, to Liesl and Friedrich
+#Adieu, adieu, to Liesl, Friedrich, and Louisa
+#Adieu, adieu, to Liesl, Friedrich, Louisa, and Kurt
+#Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, and Brigitta
+#Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, and Marta
+#Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, Marta, and Gretl
