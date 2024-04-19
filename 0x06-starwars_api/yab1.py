8c8be9ee9e5a6a0fe4020c088ef6f1472b4d7e48
Yabiti <yabetseyohannes@gmail.com>
2024-04-19 10:37:19 +0300

    .

--- 0x06-starwars_api/yab1.py
+++ 0x06-starwars_api/yab1.py
@@ -1,4 +1,3 @@
-import inflect
 #Adieu, adieu, to Liesl
 #Adieu, adieu, to Liesl and Friedrich
 #Adieu, adieu, to Liesl, Friedrich, and Louisa
@@ -6,7 +5,13 @@ import inflect
 #Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, and Brigitta
 #Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, and Marta
 #Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, Marta, and Gretl
-
+import inflect
 p = inflect.engine()
-
-text = ["adieu", "adieu"]
+text = ["Adieu", "adieu"]
+while True:
+    try:
+        name = input("name: ")
+    except EOFError:
+        print()
+        break
+    text.append(name)
