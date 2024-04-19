780a69c73f3429d27f23d16080bf508081c48b98
Yabiti <yabetseyohannes@gmail.com>
2024-04-19 07:46:34 +0300

    .

--- 0x06-starwars_api/yab.py
+++ 0x06-starwars_api/yab.py
@@ -6,10 +6,24 @@ figlet = Figlet()
 
 na = figlet.getFonts()
 
+try:
+    f = input("input: ")
+    figlet.setFont(font=random.choice(na))
+    print(na)
+except IndexError:
+    print("Error")
+
 if len(sys.argv) == 0:
     figlet.setFont(font=random.choice(na))
 elif len(sys.argv) == 2:
     for s in na:
         print(figlet.renderText(s))
 else:
-    sys.exit("Error")
+    figlet.setFont(font=random.choice(na))
+
+try:
+    f = input("input: ")
+    figlet.setFont(font=random.choice(na))
+    print(na)
+except IndexError:
+    print("Error")
