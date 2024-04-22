1d01986a123105cd320da717cfc3c4c2ef2fc7ef
Yabiti <yabetseyohannes@gmail.com>
2024-04-22 10:46:11 +0300

    /

--- 0x06-starwars_api/yab3.py
+++ 0x06-starwars_api/yab3.py
@@ -2,6 +2,12 @@ import requests
 import sys
 
 amount = 345465
-
+amount = float(amount * 2)
+print(amount)
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json" + sys.argv[1])
 print(response.json())
+
+try:
+    amount = float()
+except requests.RequestException:
+    sys.exit("not a command line argument")
