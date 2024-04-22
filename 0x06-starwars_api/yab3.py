34224559cd28cd1dd0f940b7dcb86e6cf3a962f2
Yabiti <yabetseyohannes@gmail.com>
2024-04-22 10:41:14 +0300

    .

---
+++ 0x06-starwars_api/yab3.py
@@ -0,0 +1,7 @@
+import requests
+import sys
+
+amount = 345465
+
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json" + sys.argv[1])
+print(json.dumps(response.json(), indnet = 2))
