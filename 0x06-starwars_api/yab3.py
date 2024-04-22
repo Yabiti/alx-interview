570f3cb0e395c2a894763aaed76ed53809a63d6d
Yabiti <yabetseyohannes@gmail.com>
2024-04-22 10:26:23 +0300

    .

--- 0x06-starwars_api/yab3.py
+++ 0x06-starwars_api/yab3.py
@@ -2,3 +2,11 @@ import requests
 import sys
 
 n = 38,761.0833
+
+try:
+    if len(sys.argv) != float:
+        sys.exit()
+    response = requests.get("https://api.coindesk.com/v1/bpi/cuuremtprice.json")
+    print(response.json())
+except requests.RequestException:
+    sys.exit()
