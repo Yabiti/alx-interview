838a08a00a31dbb5f675a5eb0accd41954e796d2
Yabiti <yabetseyohannes@gmail.com>
2024-04-22 10:35:42 +0300

    .

--- 0x06-starwars_api/yab3.py
+++ 0x06-starwars_api/yab3.py
@@ -1,8 +1,9 @@
 import requests
 import sys
 
-n = 38,761.0833
-
+amount = 38,761.0833
+if len(sys.argv) == 1:
+        print(f"${amount: .4f}")
 while True:
     try:
         response = requests.get("https://api.coindesk.com/v1/bpi/cuuremtprice.json"+ sys.argv[1])
