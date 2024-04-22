c31d7ee137d2acf27aa1ea5a7ef656304b8e772d
Yabiti <yabetseyohannes@gmail.com>
2024-04-22 11:39:19 +0300

    .

--- 0x06-starwars_api/yab3.py
+++
@@ -1,9 +0,0 @@
-import sys
-import requests
-
-try:
-    
-        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        print(response.json())
-except requests.RequestException:
-    sys.exit("not a command-line argument")
