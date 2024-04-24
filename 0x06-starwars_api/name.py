f378ab59aa50e15b940f0751fc4bfd8525b4cb8a
Yabiti <yabetseyohannes@gmail.com>
2024-04-24 14:23:29 +0300

    .

--- 0x06-starwars_api/name.py
+++ 0x06-starwars_api/name.py
@@ -1,1 +1,9 @@
 import csv
+
+students = []
+
+with open("students.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"]}, {"home"}: row["home"]):
+        
