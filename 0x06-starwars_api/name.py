52dcea84ed707277c1d89d6ab7eda4ba6a9f5147
Yabiti <yabetseyohannes@gmail.com>
2024-04-24 14:07:32 +0300

    .

--- 0x06-starwars_api/name.py
+++ 0x06-starwars_api/name.py
@@ -3,3 +3,6 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split()
+        student = {}
+        student["name"] = name
+        student["house"] = house
