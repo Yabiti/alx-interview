85a8575167c2c9544d6b376cbb8ad898ed60d1ea
Yabiti <yabetseyohannes@gmail.com>
2024-04-24 14:28:55 +0300

    .

--- 0x06-starwars_api/name.py
+++ 0x06-starwars_api/name.py
@@ -5,6 +5,6 @@ students = []
 with open("students.csv") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        students.append({"name": row["name"]}, {"home": row["home"]})
+        students.append({"name": row["name"], "home": row["home"]})
     for student in sorted(students, key=lambda student: student["home"]):
-        print(f"{student["name"]} is in {student["home"]}")
+        print(f"{student['name']} is in {student['home']}")
