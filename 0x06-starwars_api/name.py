9bc366c3be42908e196dfd41967049109f72e781
Yabiti <yabetseyohannes@gmail.com>
2024-04-24 14:52:06 +0300

    .

--- 0x06-starwars_api/name.py
+++ 0x06-starwars_api/name.py
@@ -3,6 +3,6 @@ import csv
 name = input("what;s your name? ")
 home = input("where do you live? ")
 
-with open("students.csv") as file:
+with open("students.csv", "a") as file:
     writer = csv.DictWriter(file, fieldnames=["name", "home"])
     writer.writerow({"name": name, "home": home})
