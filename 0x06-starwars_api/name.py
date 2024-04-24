9d635c604d5bf97c8e6955000bf9f72f03d1973a
Yabiti <yabetseyohannes@gmail.com>
2024-04-24 14:14:16 +0300

    .

--- 0x06-starwars_api/name.py
+++ 0x06-starwars_api/name.py
@@ -4,3 +4,6 @@ with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split()
         students.append(f"{name} is in {house}")
+
+    for student in sorted(students):
+        print(student)
