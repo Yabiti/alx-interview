3d7b30b5b42e0ea85cb15bfca35114db927b90fb
Yabiti <yabetseyohannes@gmail.com>
2024-04-24 14:05:37 +0300

    .

--- 0x06-starwars_api/name.py
+++ 0x06-starwars_api/name.py
@@ -1,12 +1,12 @@
 students = []
 
-with open(students.csv) as file:
+with open("students.csv") as file:
     for line in file:
         name, home = line.rstrip().split(",")
         student = {}
         student["name"] = name
         student["home"] = home
-        student.append(student)
+        students.append(student)
 
     for student in students:
         print(f"{name} is in {home}")
