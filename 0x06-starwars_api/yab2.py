476985a80219c8891f6eed3de158788f912f8a61
Yabiti <yabetseyohannes@gmail.com>
2024-04-25 12:50:05 +0300

    .

--- 0x06-starwars_api/yab2.py
+++ 0x06-starwars_api/yab2.py
@@ -4,10 +4,15 @@ import random
 i = random.randint(1,100)
 i = 1
 n = int(input("level: "))
+n = int(input("guess: "))
 while i < 100:
     if n < 0:
         n = int(input("level: "))
-    if i < n:
-        sys.argv("Too small")
+    if n < i:
+        sys.exit("Too small")
     if i == n:
-        sys.argv("just right!")
+        sys.exit("just right!")
+    if n > i:
+        sys.exit("Too large")
+    else:
+        print(input("Level: "))
